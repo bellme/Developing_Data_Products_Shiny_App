08a6f8831a6cb1bda0d3478e6d687fdc997f563b
Bell <mabell@aus2m-mabell.local>
2015-05-23 13:35:28 -0500

    Added Documentation

--- server.R
+++ server.R
@@ -1,45 +1,62 @@
+## Creates linear model using features and polynomial order chosen by user and computes model coefficients 
+## Linear model of gas mileage is built on these features, their statistical relevance, and a graphical 
+## representation of goodness of fit using the mtcars data set are returned through ui.R
+
+## feature options on which mtcars$mpg will be modeled
+##    - mtcars$cyl  - # cylinders
+##    - mtcars$hp   - horsepower
+##    - mtcars$wt   - weight
+
+## features will be fit with either first or first and second order polynomial terms depending on
+## user input
+
+## Basic instruction on how to use the app and what the outputs are is included in the app itself
+
 require(ggplot2)
 data(mtcars)
 shinyServer( 
   function(input, output, session) {
     
     output$text1 <- renderText({ 
+      ## Verify user input
       paste("You have selected feature ", input$features,". ")
     })
     
     output$text2 <- renderText({ 
+      ## Verify user input
       paste("You have selected polynomial of order ", input$order,".")
     })
-    
+    ## Create reactive function to generate model
     mdl <- reactive({
       
       if (input$order==1) {
-        
+        ## linear model formula with first order terms for input features
         mdlFormula <- as.formula(paste("mpg ~ ", paste(input$features, collapse="+")))
                                  
       }  else if (input$order==2) {
-        
+        ## first order terms for input features
         firstOrder <- paste("mpg ~ ", paste(input$features, collapse="+"))
         
         n <- length(input$features)
         secondOrder <- c("")
         
         for (i in 1:n) {
-          
+          ## second order terms for input features
           secondOrder <- paste(secondOrder,"+ I(", input$features[i],"^2)")
           
         }
-      
+        ## linear model formula with first and second order terms for input features
         mdlFormula <- as.formula(paste(firstOrder, secondOrder))
       
       }
-      
+      ## compute linear model
       lm(mdlFormula, data=mtcars)
       
     })
     
     output$mdlTable <- renderTable({
       if(!is.null(input$features)){
+        ## output summary of model coefficients, values, stdevs, t-statistics and p values
         summary(mdl())$coefficients
       } else {
         print(data.frame(Warning="Please select Model Features."))
@@ -48,6 +65,9 @@ shinyServer(
     
     output$myPlot <- renderPlot({
       if(!is.null(input$features)) {
+        ## once features have been selected, output a plot of predicted values from model vs
+        ## actual values from mtcars data set - include dotted red line to indicate
+        ## where the predicted values would equal the observed values
         x <- data.frame(mtcars$mpg, mdl()$fitted.values)
         names(x) <- c("mpg","pred")
        g <- ggplot(aes(x=mpg, y=pred), data=x) + geom_point() + ylab("Predicted MPG") + xlab("Observed MPG") 
--- ui.R
+++ ui.R
@@ -1,44 +1,66 @@
+## Creates UI that takes features and polynomial order chosen by user and outputs model coefficients 
+## of a linear model of gas mileage on these features, their statistical relevance, and a graphical 
+## representation of goodness of fit using the mtcars data set
+
+## feature options on which mtcars$mpg will be modeled
+##    - mtcars$cyl  - # cylinders
+##    - mtcars$hp   - horsepower
+##    - mtcars$wt   - weight
+
+## features will be fit with either first or first and second order polynomial terms depending on
+## user input
+
+## Basic instruction on how to use the app and what the outputs are is included in the app itself
+
+
 shinyUI(fluidPage( 
   titlePanel("Simple Linear Models of Gas Mileage"), 
   
   fluidRow(
     column(4,
+           ## Instructions on choosing features
            h4("Please select features on which to model gas mileage using the mtcars dataset."),
            wellPanel(
-             checkboxGroupInput("features", "Feature options",
-                              c("# Cylinders" = "cyl",
+             checkboxGroupInput("features", "Feature options", ## creates check box in which user
+                              c("# Cylinders" = "cyl",         ## selects model features
                                 "Horsepower" = "hp",
                                 "Weight" = "wt"))
        )
     ),
     
     column(4,
+           ## Instructions on choosing order of polynomial
            h4("Please select order of polynomial terms to include for chosen features."),
            wellPanel(
-              radioButtons("order", label = h3("Order of Polynomial"),
-                choices = list("1" = 1, "2" = 2))
-                 )
+              radioButtons("order", label = h3("Order of Polynomial"), ## creates radio button box
+                choices = list("1" = 1, "2" = 2))                      ## in which user selects order
+                 )                                                     ## of polynomial terms
       ),
   
     column(8,
+           ## returns user input for verification
          textOutput("text1")
 ),
 
     column(8,
+           ## returns user input for verification
          textOutput("text2")
     )
 
 ),
 
-    column(6,
-           h4("Model coefficents and significance levels"),
+    column(8,
+           ## Explanation of table output
+           h4("Model coefficents, standard errors, t-statistics, and p-values"),
            wellPanel(
               tableOutput('mdlTable')
             )
            ),
 
     column(8,
-           h4("Goodness of model fit: Predicted vs Observed Values of MPG"),
+           ## Explanation of graphical output
+           h4("Goodness of model fit: Predicted vs Observed Values of MPG"), 
+           h5("(Dotted line indicates where predicted values would equal observed values of MPG)"),
          plotOutput('myPlot')
   ) 
  )
